5acce655a604f2b79607089f956856b0391e8c39
eRui Design <50480764+eruidesign@users.noreply.github.com>
2022-10-18 19:08:19 +0800

    Product

--- woocommerce/title.php
+++ woocommerce/title.php
@@ -21,3 +21,16 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 the_title( '<h1 class="product_title entry-title">', '</h1>' );
 
+/*global $product;
+    $id = $product->get_id();
+
+    $terms = get_the_terms( $id, 'product_cat' );
+    $current_term_ID = $terms[0]->term_id;
+    $current_term_slug = $terms[0]->slug;
+    $parent_term_ID = get_term_by('slug','wam','product_cat')->term_id;*/
+
+    if ( in_array( $current_term_slug, array('seasons','animals','numbers','colors','emotions') ) ) {
+        echo '<a href="#" class="button"><span></span> Play Audio Sample</a>';
+    }else{
+        echo '<a href="#" class="button"><span></span> Artist Profile</a>';
+    }
